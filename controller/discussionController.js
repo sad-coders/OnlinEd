fd15d4a71d38738b29b539191905e9fbf681659c
akash-007-kotnala <ghostmankotnala29@gmail.com>
2022-04-17 17:40:07 +0530

    Discussion Router added

--- controller/discussionController.js
+++ controller/discussionController.js
@@ -1,95 +1,139 @@
 const db = require("../model/db.js");
 const COLLECTION_NAME = "discussion";
 
-exports.getAlldiscussion = async (req, res, next) => {
-  const connection = db.getConnection();
-  var discussions = await connection
-    .collection(COLLECTION_NAME)
-    .find({})
-    .toArray();
+// new routes.
 
-  console.log(discussions);
-  res.status(200).json({ status: "success", discussions });
+exports.getAllQuestionOfClassRoom = async (req, res, next) => {
+  try {
+    const connection = db.getConnection();
+
+    let classRoomId = req.params.classRoomId;
+
+    // find on the basis of type ="question " and on classRoomId. in decending order.
+    // return array of objects.
+    var allQuestions = await connection
+      .collection(COLLECTION_NAME)
+      .find({ classroomId: classRoomId, type: "question" })
+      .sort({ date: -1 })
+      .toArray();
+
+    console.log(allQuestions);
+    res.status(200).json({ status: "success", allQuestions });
+  } catch (error) {
+    res.status(500).send({ message: "Failed" });
+  }
 };
 
-exports.getByAuthorId = async (req, res, next) => {
-  const connection = db.getConnection();
+exports.addQuestion = async (req, res, next) => {
+  try {
+    const connection = db.getConnection();
+    var question = req.body.question;
 
-  let authorId = req.params.AuthorId;
+    console.log(question);
+    //set at backend.
+    // question.parentId = "";
+    // question.type = "question";
 
-  var discussions = await connection
-    .collection(COLLECTION_NAME)
-    .find({ authorId: authorId })
-    .sort({ date: -1 })
-    .toArray();
+    var insertQuestion = await connection
+      .collection(COLLECTION_NAME)
+      .insertOne(question);
+    console.log(insertQuestion);
 
-  console.log(discussions);
-  res.status(200).json({ status: "success", discussions });
+    //insertQuestion will contain the _id. for the forntend
+    res.status(200).json({ status: "success", insertQuestion });
+  } catch (error) {
+    res.status(500).send({ message: "Failed" });
+  }
 };
 
-exports.insertdiscussion = async (req, res, next) => {
-  const connection = db.getConnection();
-  var discussion = req.body.discussion;
+// question can only deleted by the id with the same autherid.
+exports.deleteQuestion = async (req, res, next) => {
+  try {
+    const connection = db.getConnection();
+    const { studentId, authorId, _id } = req.body.question;
 
-  console.log(discussion);
-  var insertDiscussion = await connection
-    .collection(COLLECTION_NAME)
-    .insertOne(discussion);
+    console.log(studentId, authorId);
 
-  console.log(insertDiscussion);
-  res.status(200).json({ status: "success", insertDiscussion });
+    if (studentId === authorId) {
+      //delete the question.
+      var deletequestion = await connection
+        .collection(COLLECTION_NAME)
+        .deleteMany({ _id: _id });
+
+      console.log(deletequestion);
+      // delete the answer related to it.
+      var deleteRelatedAnswer = await connection
+        .collection(COLLECTION_NAME)
+        .deleteMany({ parentId: _id });
+      console.log(deleteRelatedAnswer);
+
+      res.status(200).json({ status: "success", deletequestion });
+    } else {
+      res.status(400).send({ message: "Invalid User to Delete the Question" });
+    }
+    // res.status(200).json({ status: "success", deletequestion });
+  } catch (error) {
+    res.status(500).send({ message: "Failed" });
+  }
 };
 
-exports.updatediscussion = async (req, res, next) => {
-  const connection = db.getConnection();
+// for answer.
+exports.getAllAnswerOfQuestion = async (req, res, next) => {
+  try {
+    const connection = db.getConnection();
 
-  let {
-    _id,
-    type,
-    date,
-    content,
-    authorId,
-    parentId,
-    likecount,
-    dislikecount,
-  } = req.body.discussion;
+    let questionId = req.params.questionId;
+    let classRoomId = req.params.classRoomId;
 
-  var updatedDiscussion = await connection
-    .collection(COLLECTION_NAME)
-    .updateOne(
-      { _id: _id },
-      {
-        $set: {
-          type: type,
-          date: date,
-          content: content,
-          authorId: authorId,
-          parentId: parentId,
-          likecount: likecount,
-          dislikecount: dislikecount,
-        },
-      }
-    );
+    console.log(questionId, classRoomId);
+    var allAnswers = await connection
+      .collection(COLLECTION_NAME)
+      .find({ classroomId: classRoomId, parentId: questionId, type: "answer" })
+      .sort({ date: -1 })
+      .toArray();
 
-  console.log(updatedDiscussion);
-
-  res.status(200).json({ status: "success", updatedDiscussion });
+    console.log(allAnswers);
+    res.status(200).json({ status: "success", allAnswers });
+  } catch (error) {
+    res.status(500).send({ message: "Failed" });
+  }
 };
 
-exports.deletedicussion = async (req, res, next) => {
-  const connection = db.getConnection();
-  var discussion = req.body.discussion;
-  let { _id, authorId, parentId } = discussion;
+exports.addAnswer = async (req, res, next) => {
+  try {
+    const connection = db.getConnection();
+    var answer = req.body.answer;
 
-  console.log(_id, authorId, parentId);
+    console.log(answer);
 
-  var discussionId = _id;
+    var insertAnswer = await connection
+      .collection(COLLECTION_NAME)
+      .insertOne(answer);
 
-  var deleteDiscussion = await connection
-    .collection(COLLECTION_NAME)
-    .deleteMany({ _id: discussionId });
+    console.log(insertAnswer);
+    res.status(200).json({ status: "success", insertAnswer });
+  } catch (error) {
+    res.status(500).send({ message: "Failed" });
+  }
+};
 
-  console.log(deleteDiscussion);
+exports.deleteAnswer = async (req, res, next) => {
+  try {
+    const connection = db.getConnection();
+    const { _id, studentId, authorId } = req.body.answer;
 
-  res.status(200).json({ status: "success", deleteDiscussion });
+    if (studentId === authorId) {
+      var deleteanswer = await connection
+        .collection(COLLECTION_NAME)
+        .deleteMany({ _id: _id });
+
+      res.status(200).json({ status: "success", deleteanswer });
+    } else {
+      res.status(400).send({ message: "Invalid User to Delete the Answer" });
+    }
+
+    // res.status(200).json({ status: "success", deleteanswer });
+  } catch (error) {
+    res.status(500).send({ message: "Failed" });
+  }
 };
