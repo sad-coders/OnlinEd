d77c60a938756bfd9b10c50979f378dad3ea4a68
Dushyanth <kd13@iitbbs.ac.in>
2022-04-22 16:45:42 +0530

    create classroom

--- controller/classroomController.js
+++ controller/classroomController.js
@@ -19,7 +19,7 @@ exports.createClassroom = async (req, res, next) => {
   const connection = db.getConnection()
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
   var yyyy = today.getFullYear();
   today = dd + '-' + mm + '-' + yyyy;
 
@@ -28,12 +28,24 @@ exports.createClassroom = async (req, res, next) => {
     classcode: crypto.randomUUID(),
     authorId: '626126cdd3990228bb87b725' /**TODO: author id */,
     createdAt: today,
-    className: req.body.className 
+    className: req.body.className
   }
+  const personId = req.body.personId;
   //console.log(new_classroom)
   const saved_classroom = await connection.collection(COLLECTION_NAME).insertOne(new_classroom)
-  //console.log("create classroom",saved_classroom)
-  res.status(201).send()
+  //console.log("create classroom", saved_classroom)
+  var updatedPerson = await connection.collection('person')
+    .findOneAndUpdate(
+      { _id: personId }, { $push: { classrooms: {
+        classroomId : saved_classroom.insertedId,
+        authorName : "Sripad", /**TODO: author name */
+        className : new_classroom.className
+      } } },
+      { returnDocument: 'after', returnNewDocument: true }
+    );
+  //console.log("create classroom updated person", updatedPerson);
+  updatedPerson = updatedPerson.value;
+  res.status(201).send();
 }
 exports.getClassroomsOfPerson = async (req, res, next) => {
   //console.log(req.query.email)
