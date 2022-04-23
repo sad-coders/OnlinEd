ca6f6b8e46ad490a0d89a69ce313698ad830d0f3
TVSSSRIPAD <tss11@iitbbs.ac.in>
2022-04-23 20:59:09 +0530

    added cors handler middleware

--- index.js
+++ index.js
@@ -15,6 +15,28 @@ console.log(process.env.AZURE_COSMOSDB_URL);
 const solution = require("./routes/solutionRouter.js");
 const discussion = require("./routes/discussionRouter.js");
 
+// middleware to allow CORS access
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://onlined-fe.azurewebsites.net/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 // testing purpose.
 app.use("/test", (req, res) => {
   console.log(req);
